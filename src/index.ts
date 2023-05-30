ac5a4bde35b676f92c44bbbad1914b8845e7eeb1
unknown <sliver.smithbs@gmail.com>
2023-05-29 21:49:16 -0300

    Commit Alteração Index

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import { Disciplina } from "./disciplina/disciplina";
 
 const end01: Endereco = new Endereco('CEP: 49.300-000', 'Rua: A', 15, 'Bairro: Centro', 'Cidade: Tobias Barreto', 'Estado: Sergipe');
 const end02: Endereco = new Endereco('CEP: 49.300-000', 'Rua: B', 118, 'Bairro: Centro', 'Cidade: Tobias Barreto', 'Estado: Sergipe');
+const end03: Endereco = new Endereco('CEP: 49.300-000', 'Rua: C', 63, 'Bairro: Padre Pedro', 'Cidade: Tobias Barreto', 'Estado: Sergipe');
 
 const nota01: Nota = new Nota(10, 4);
 const nota02: Nota = new Nota(8, 2);
@@ -15,16 +16,22 @@ const nota02: Nota = new Nota(8, 2);
 const nota03: Nota = new Nota(8, 2);
 const nota04: Nota = new Nota(10, 4);
 
-const disc01: Disciplina = new Disciplina("Programação II", 100, [nota01, nota02])
-const disc02: Disciplina = new Disciplina("Banco de Dados", 80, [nota03, nota04]);
+const nota05: Nota = new Nota(6, 4);
+const nota06: Nota = new Nota(8, 2);
 
-const hesnan: Aluno = new Aluno("Feminino", "059.395.487-21", "Hesnan", "Mendes", 69, true, [end01, end02] , ["79 9.9878-4521"], "Comércio", SituacaoAluno.ATIVO, [disc01]);
+const disc01: Disciplina = new Disciplina("Programação II", 100, [nota01, nota02]);
+const disc02: Disciplina = new Disciplina("Banco de Dados", 80, [nota03, nota04]);
+const disc03: Disciplina = new Disciplina("Construção de Sites I", 72, [nota05, nota06]);
+
+const hesnan: Aluno = new Aluno("Feminino", "059.395.487-21", "Hesnan", "Mendes", 69, true, [end01, end02], ["79 9.9878-4521"], "Comércio", SituacaoAluno.ATIVO, [disc01]);
 const jahell: Aluno = new Aluno("Masculino", "741.666.346-55", "JaHell", "Signorini Filho", 66, true, [new Endereco('CEP: 49.300-000', 'Rua: B', 11, 'Bairro: Santa Rita', 'Cidade: Tobias Barreto', 'Estado: Sergipe')], ["79 9.8671-2347"], "Informática", SituacaoAluno.ATIVO, [disc02]);
+const werik: Aluno = new Aluno("Masculino", "123.654.789-66", "Werik", "Jahell", 66, true, [end03], ["79 9.8834-3565"], "RH", SituacaoAluno.CONCLUIDO, [disc03]);
 const bricio: Professor = new Professor("Masculino", "745.896.312-11", "Bricio", "Musck", 33, true,[new Endereco('CEP: 49.300-000', 'Rua: C', 7, 'Bairro: Walter Franco', 'Cidade: Tobias Barreto', 'Estado: Sergipe')], ["79 9.4571-82"], 666999);
 
 
 console.log(hesnan);
 console.log(jahell);
+console.log(werik);
 console.log(bricio);
 
 console.log(hesnan.disciplinas[0].calcularMediaAritmetica());
@@ -33,9 +40,18 @@ console.log(hesnan.disciplinas[0].calcularMediaPonderada());
 console.log(jahell.disciplinas[0].calcularMediaAritmetica());
 console.log(jahell.disciplinas[0].calcularMediaPonderada());
 
+console.log(werik.disciplinas[0].calcularMediaAritmetica());
+console.log(werik.disciplinas[0].calcularMediaPonderada());
+
 
 hesnan.submeterProjetoPesquisa();
 hesnan.submeterProjetoExtensao();
+
+jahell.submeterProjetoPesquisa();
+jahell.submeterProjetoExtensao();
+
+werik.submeterProjetoPesquisa();
+werik.submeterProjetoExtensao();
 
 bricio.submeterProjetoPesquisa();
 bricio.submeterProjetoExtensao();
